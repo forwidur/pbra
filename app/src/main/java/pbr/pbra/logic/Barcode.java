a3f01d563378add079094fa7940e5bb5318fb3e4
Max Grigorev <forwidur@gmail.com>
2018-08-20 04:43:04 -0700

    Cleans up barcode stripping logic.

--- app/src/main/java/pbr/pbra/logic/Barcode.java
+++ app/src/main/java/pbr/pbra/logic/Barcode.java
@@ -1,21 +1,21 @@
 package pbr.pbra.logic;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class Barcode {
-  private static Pattern qr = Pattern.compile("HTTPSWWWBLUEGOGOCOMQRCODEHTMLNO00.*(\\d{7})");
-
-  private static Pattern newBar = Pattern.compile("0{5}(\\d{4})");
+  private static ArrayList<Pattern> ps = new ArrayList<Pattern>() {{
+    add(Pattern.compile("HTTPSWWWBLUEGOGOCOMQRCODEHTMLNO00.*(\\d{7})"));
+    add(Pattern.compile("https://www\\.bluegogo\\.com/qrcode\\.html\\?no=00.*(\\d{7})"));
+    add(Pattern.compile("0{5}(\\d{4})"));}};
 
   public static String strip(String c) {
-    Matcher m = qr.matcher(c);
-    if(m.matches()) {
-      return m.group(1);
-    }
-    m = newBar.matcher(c);
-    if(m.matches()) {
-      return m.group(1);
+    for (Pattern p : ps) {
+      Matcher m = p.matcher(c);
+      if(m.matches()) {
+        return m.group(1);
+      }
     }
     return c;
   }
