5a90cac56ba1385e83a864046eb5d0648c098c14
Max Grigorev <forwidur@gmail.com>
2018-08-21 01:42:49 -0700

    Cleans up the Barcode class.

--- app/src/main/java/pbr/pbra/logic/Barcode.java
+++ app/src/main/java/pbr/pbra/logic/Barcode.java
@@ -5,10 +5,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class Barcode {
-  private static ArrayList<Pattern> ps = new ArrayList<Pattern>() {{
-    add(Pattern.compile(".*ODEHTMLNO00.*(\\d{7})"));
-    add(Pattern.compile("https://www\\.bluegogo\\.com/qrcode\\.html\\?no=00.*(\\d{7})"));
-    add(Pattern.compile("0+(\\d{4})"));}};
+  private final static Pattern[] ps = {
+    Pattern.compile(".*ODEHTMLNO00.*(\\d{7})"),
+    Pattern.compile("https://www\\.bluegogo\\.com/qrcode\\.html\\?no=00.*(\\d{7})"),
+    Pattern.compile("0+(\\d{4})")};
 
   public static String strip(String c) {
     for (Pattern p : ps) {
